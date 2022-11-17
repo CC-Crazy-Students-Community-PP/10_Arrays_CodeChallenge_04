260181fcd7eb25935a5c4c4c5d63a74a0fdd2b77
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-17 11:11:41 +0100

    Code Challange 04 Arrays push() / pop()

--- arr_01.js
+++ arr_01.js
@@ -1,119 +1,117 @@
-
+/*********   Code Challange 04 Arrays push() / pop()  **********/
 
 /*
-Aufgabe:
+    Aufgabe:
+        <html><head></head><body><h1></h1><p></p></body></html>
+        ---->
+        <html>
+            <head>
+            </head>
+            <body>
+                <h1>
+                </h1>
+                <p>
+                </p>
+            </body>
+        </html>
 
-<html><head></head><body><h1></h1><p></p></body></html>
-	
----->
+        --> return : "\n"  
+        --> Tab: "\t"
 
-<html>
-	<head>
-	</head>
-	<body>
-		<h1>
-        </h1>
-		<p>
-        </p>
-	</body>
-</html>
-
---> return : "\n"  
---> Tab: "\t"
-
-Verwenden Sie dafür die untenstehenden Arrays
+        Verwenden Sie dafür die untenstehenden Arrays
 */
 
-const controls = ["<", "</", ">"];
-const tags = [  
-                "html","head","head","body",
-                "h1","h1",
-                "p","p",
-                "ul","li","li","li","li","ul",
-                "h1","h1",
-                "p","p",
-                "ul","li","li","li","li","ul",
-                "body","html"
-            ];
+/********************************************************/
+/*******                Example 01                *******/
+/********************************************************/
 
-            // "h1","h1",
-            // "p","p",
-            // "ul","li","li","li","li","li","li","ul",
-            // "p","p",
+    // Modul: globale Variablen
+    const cobj = { 
+        open_o: "<", 
+        close_o: "</", 
+        close: ">" 
+    }
+    const newLine = "\n";
+    const controls = [ "<", "</", ">" ];
+    const tags = [ 
+        "html", "head", "head", "body",
+        "h1", "h1",
+        "p", "p",
+        "ul", "li", "li", "li", "li", "li", "li", "ul",
+        "p", "p",
+        "body", "html"
+    ];
 
-
-
-let stack = [];
+    let stack = [];
 
 // Modul: HTML-Synthese | Test
-// output(getHTML());
-function getHTML() {
+    output( getHTML() );
+    function getHTML() {
+        let htmlStr = "", op = "", count = 0;
 
-    let htmlStr = "";
-	let count = 0;
-	let op = "";
-	const newLine = "\n";
+        for ( let i = 0; i < tags.length; i ++ ) {
+            // if(false) {
+            if ( isOpenElement( tags[ i ] ) ) { 
+                count = stack.length - 1; 
+                op = "open";
+            } else {
+                count = stack.length; 
+                op = "close";
+            }
+            htmlStr += getTabs( count ) + getElement( tags[i], op) + newLine;
+        }
 
-    for (let i = 0; i < tags.length; i++) {
-        if (isOpenElement(tags[i])) { 
-            count = stack.length -1; 
-			op = "open";
-        } else {
-			count = stack.length; 
-			op = "close";
-		}
-
-		htmlStr += getTabs(count) + getElement(tags[i],op) + newLine;
-
+        return htmlStr;
     }
 
-    return htmlStr;
-}
-
-// Modul: Erstellen der Tabulatoren (1..n)
-function getTabs(count) {
-    let tabs ="";
-    for (let i = 0; i < count; i++) {
-        tabs += "\t";
+    // output( isOpenElement( "head" ) );
+    // output( isOpenElement( "body" ) );
+    // output( isOpenElement( "body" ) );
+    // output( isOpenElement( "head" ) );
+    function isOpenElement( tag ) {
+        let cond = ( tag != stack[ stack.length - 1 ] );    // tag liegt nicht oben! --> neu, open
+        
+        if ( cond ) {                                       // open
+            stack.push( tag );
+            // output( stack );                               // zeigt die Struktur in der Konsole
+            return true;
+        } else {                                            // close
+            stack.pop();
+            // output( stack );                               // zeigt die Struktur in der Konsole
+            return false;
+        }
+        
     }
-    return tabs;
-}
-
-// Modul: Entscheidung open/close
-function isOpenElement(tag) {
-    
-    // tag liegt NICHT oben! --> neu, open
-    let cond = (tag != stack[stack.length-1])  
-    
-    if (cond) {  // open
-        stack.push(tag);
-      //  output(stack);
-        return true;
-    } else {  // close
-        stack.pop();
-     //   output(stack);
-        return false;
-    }
-     
-}
 
 // Modul: Zusammenbau der Elements: <tagStr> --> Tests:
-output(getElement(tags[1],"open"));
-output(getElement(tags[1],"close"));
-output(getElement(tags[1]));
-function getElement(tag,op) {
-    switch (op) {
-        case "open": 
-            return controls[1] + tag + controls[3];
-        case "close":
-            return controls[1] + tag + controls[2];
-        default:
-          return "#!?";
+    // output(getElement(tags[1],"open"));
+    // output(getElement(tags[1],"close"));
+    // output(getElement(tags[1]));
+    function getElement( tag, op ) {
+        switch ( op ) {
+            case "open": 
+                // return controls[ 0 ] + tag + controls[ 2 ];
+                return cobj.open_o + tag + cobj.close;
+            case "close":
+                // return controls[ 1 ] + tag + controls[ 2 ];
+                return cobj.close_o + tag + cobj.close;
+            default:
+            return "#!?";
+        }
     }
-}
 
-// Modul: Ausgabe | Test
-//output("hi");
-function output(inputData) {
-    console.log(inputData);
-}
+// Modul: Erstellen der Tabulatoren (1..n)
+    function getTabs( count ) {
+        let tabs ="";
+        for ( let i = 0; i < count; i ++ ) {        // zählt die tabs
+            tabs += "\t";                           // kombiniert alle tabs aus
+        }
+        return tabs;
+    }
+
+/********************************************************/
+/*******                  Output                  *******/
+/********************************************************/
+// output
+// output( "Test" );
+function output( outputStr ) { console.log( outputStr ); }
